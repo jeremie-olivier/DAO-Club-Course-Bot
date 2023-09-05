43aa4866737137e63e8af1a1784010426f1a8457
Jeremie <jeremie.olivier.pro@gmail.com>
2023-09-05 04:06:02 +0200

    input DAO Club questions

--- prisma/seed/data/discordMsg.ts
+++ prisma/seed/data/discordMsg.ts
@@ -1,22 +1,20 @@
 export const discordMsg: any = [
   {
     slug: "onboarding",
-    content: `Hey! Welcome to the DAO Club course! 👋 \n\nIn this course you will learn how to start your own DAO. 🚀 \n\nThe lessons will cover:\n\nLesson 1 - Purpose & Mission\nLesson 2 - Strategy\nLesson 3 - Membership\nLesson 4 - Structure\nLesson 5 - Resources\nLesson 6 - Governance\nLesson 7 - Compensation\nLesson 8 - Information Management\n\n`,
+    content: `
+    Hey! Welcome to the DAO Club course! 👋 \n\n
+In this course you will learn how to start your own DAO. 🚀 \nThe lessons will cover:\n- Lesson 1 - Purpose & Mission\n- Lesson 2 - Strategy\n- Lesson 3 - Membership\n- Lesson 4 - Structure\n- Lesson 5 - Resources\n- Lesson 6 - Governance\n- Lesson 7 - Compensation\n- Lesson 8 - Information Management\n\nhttps://www.youtube.com/watch?v=_dNDWD-tUik`,
   },
   {
     slug: "wrong-response",
-    content: `Oops, nope that's not the correct answer. 😬
-
-    Try again... `,
+    content: `
+    Oops, nope that's not the correct answer. 😬\n\nTry again... `,
   },
 
   {
     slug: "lesson-part-1",
-    content: `Welcome to Lesson {{lessonOrder}}  : {{lessonName}}. 🙌 
-
-    Please watch this video below and complete the questions that follow. 👀 
-    
-    {{lessonVideoUrl}}
+    content: `
+    Welcome to Lesson {{lessonOrder}}  : {{lessonName}}. 🙌 \n\nPlease watch this video below and complete the questions that follow. 👀 \n\n{{lessonVideoUrl}}
     `,
   },
   {
